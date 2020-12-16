beb20420f04f5af07902ac217fe82829096a1c9f
eliasd1 <elidadoush23@gmail.com>
2020-12-16 12:09:22 +0200

    added a new function

--- lab07/lab-07.js
+++ lab07/lab-07.js
@@ -1,5 +1,6 @@
 var images = ["https://www.pcgamesn.com/wp-content/uploads/2019/01/Intel-i7-9700K-CPU-900x507.jpg", "https://www.amd.com/system/files/2020-07/559424-amd-ryzen-pro-1260X709.jpg", "https://images.firstpost.com/wp-content/uploads/2019/06/amd-radeon-rx-5700-xt.jpg", "https://s.yimg.com/os/creatr-uploaded-images/2020-06/6202f1e0-b63a-11ea-8efa-c68d6b052c23"]
 var colors = ["#282828", "blue", "black"]
+var button = document.querySelector("#prompt");
 
 $("#range").on("input", function(){
     $("img").attr("src", images[$(this).val() - 1])
@@ -11,3 +12,16 @@ $("#mySelect").on("change", function(){
         alert("Background color set to default")
     }
 })
+
+button.addEventListener("click", function(){
+    var input = prompt("What is your name?")
+    checkInput(input)
+})
+
+function checkInput(input){
+    if(!Number(input)){
+        alert("Welcome " + input)
+    } else{
+        alert("Please use letters and not numbers")
+    }
+}
